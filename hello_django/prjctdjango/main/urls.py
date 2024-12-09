1c4e9944e818590acda05d21a4aa213c1aaa09d8
Victor Balaniouk <129725015+VictorBRRM@users.noreply.github.com>
2024-12-09 09:06:13 +0100

    Trying to put a background

--- hello_django/prjctdjango/main/urls.py
+++ hello_django/prjctdjango/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path('', views.main),
+    # path('about', views.about)
 ]
 
--- hello_django/prjctdjango/main/views.py
+++ hello_django/prjctdjango/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/main.html')
 
-def about(request):
-    return render(request, 'main/about.html')
+# def about(request):
+#     return render(request, 'main/about.html')
